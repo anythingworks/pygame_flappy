4344956118835c2bdd998737d234598a2574d518
flynn <flynnmeyer1@gmail.com>
2018-09-02 14:12:25 -0700

    refactoring python formatting

--- flappybird.py
+++ flappybird.py
@@ -9,88 +9,94 @@ surfaceWidth = 800
 surfaceHeight = 400
 imageHeight = 110
 
-surface = pygame.display.set_mode((surfaceWidth,surfaceHeight))
+surface = pygame.display.set_mode((surfaceWidth, surfaceHeight))
 pygame.display.set_caption('flappy_bird_clone')
 clock = pygame.time.Clock()
 
 img = pygame.image.load('png/flappybird.png')
 
+
 def replay_or_quit():
-	for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
 
-		elif event.type == pygame.KEYDOWN:
-			continue
+        elif event.type == pygame.KEYDOWN:
+            continue
 
-		return event.key
+        return event.key
 
-	return event.key
+    return event.key
+
 
 def makeTextObjs(text, font):
-	textSurface = font.render(text, True, white)
-	return textSurface, textSurface.get_rect()
-
-def msgSurface(text):
-	smallText = pygame.font.Font('freesansbold.ttf', 20)
-	largeText = pygame.font.Font('freesansbold.ttf', 150)
+    textSurface = font.render(text, True, white)
+    return textSurface, textSurface.get_rect()
 
 
-	titleTextSurf, titleTextRect = makeTextObjs(text, largeText)
-	titleTextRect.center = surfaceWidth / 2, surfaceHeight / 2
-	surface.blit(titleTextSurf, titleTextRect)
+def msg_surface(text):
+    small_text = pygame.font.Font('freesansbold.ttf', 20)
+    large_text = pygame.font.Font('freesansbold.ttf', 150)
 
-	typTextSurf, typTextRect = makeTextObjs('press any key to continue', smallText)
-	typTextRect.center = surfaceWidth / 2, ((surfaceHeight / 2) + 100)
-	surface.blit(typTextSurf, typTextRect)
+    titleTextSurf, titleTextRect = makeTextObjs(text, large_text)
+    titleTextRect.center = surfaceWidth / 2, surfaceHeight / 2
+    surface.blit(titleTextSurf, titleTextRect)
 
-	pygame.display.update()
-	time.sleep(1)
+    type_text_surf, typTextRect = makeTextObjs('press any key to continue', small_text)
+    typTextRect.center = surfaceWidth / 2, ((surfaceHeight / 2) + 100)
+    surface.blit(type_text_surf, typTextRect)
 
-	while replay_or_quit == None:
-		clock.tick()
+    pygame.display.update()
+    time.sleep(1)
 
-	# main()
+    while replay_or_quit == None:
+        clock.tick()
+
+
+# main()
 
 def gameOver():
-	msgSurface('Kaboom!')
-	quit()
+    msg_surface('Kaboom!')
+    quit()
+
 
 def bird(x, y, image):
-	surface.blit(img, (x,y))
+    surface.blit(img, (x, y))
+
 
 def main():
-	x = 50
-	y = 100
-	y_move = 0
+    x = 50
+    y = 100
+    y_move = 0
 
-	game_over = False
+    game_over = False
 
-	while not game_over:
+    while not game_over:
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				game_over = True
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP:
-					y_move = -5
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    y_move = -5
 
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_UP:
-					y_move = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    y_move = 5
 
-		y += y_move
+        y += y_move
 
-		surface.fill(black)
-		bird(x, y, img)
+        surface.fill(black)
+        bird(x, y, img)
 
-		if y > surfaceHeight - 110 or y < -70 :
-			gameOver()
+        if y > surfaceHeight - 110 or y < -70:
+            gameOver()
 
-		pygame.display.update()
-		clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
+
 
 main()
 pygame.quit()
